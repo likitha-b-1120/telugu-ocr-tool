0bdf1fcb98d8700c24ed4cd7af2234f328eb1f61
likitha-b-1120 <bhavanalikitha1120@gmail.com>
2025-06-10 11:45:09 +0530

    Fix port binding for Render deployment

--- app_pp.py
+++ app_pp.py
@@ -157,4 +157,7 @@ def process():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
